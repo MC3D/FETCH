efbf7710a12233df900c90d32cf3a528f2527e80
MC3D <m.chapman.home@gmail.com>
2018-11-02 19:23:47 -0400

    adds basic layout component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import Link from 'next/link'
+import Layout from './../components/Layout'
 
-const Index = () => (
-  <div>
-    <Link href="/friends">
-      <button>Friends</button>
-    </Link>
+export default () => (
+  <Layout>
     <p>Hello Next.js</p>
-  </div>
+  </Layout>
 )
-
-export default Index
-
-// use Link for client-side navigation
-// action takes place in the browser without making a request to the server
-// next/link does all the `location.history` handling for you
-
-// The only requirement for components placed inside a Link is they should accept an onClick prop
